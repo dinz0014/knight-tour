3fd0e01825361fc03796c8e30a5d845aa6796b9c
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2023-02-07 21:31:36 +1100

    Added logic to move the knight piece around. Need to work on this more to have better styling

--- src/components/ChessBoard/ChessBoard.tsx
+++ src/components/ChessBoard/ChessBoard.tsx
@@ -1,28 +1,65 @@
 import { SimpleGrid } from "@mantine/core";
-import { useState } from "react";
+import { useDisclosure, useMouse } from "@mantine/hooks";
+import React, { useMemo, useState } from "react";
+import { getRowColFromString, getStringFromRowCol } from "../../api/algebraNotation";
+import { checkValidMove } from "../../api/knightsTour";
 import ChessSquare from "../ChessSquare/ChessSquare";
 
 const ChessBoard = () => {
-	const [board, setBoard] = useState(() => {
-		const arr = [];
+	// Memoise the initial board state
+	const initialBoard: number[][] = useMemo(() => {
+		const board = Array.from({ length: 8 }, () => Array.from({ length: 8 }, () => 0));
+		board[0][0] = -1;
+		return board;
+	}, []);
 
-		for (let i = 0; i < 8; i++) {
-			arr.push(Array(8).fill(0));
+	// State variables
+	const [board, setBoard] = useState<number[][]>(initialBoard);
+	const [knightLocation, setKnightLocation] = useState<string>("a8");
+	const [knightSelected, handlers] = useDisclosure(false);
+
+	// Memoise the current knight position
+	const knightPosition = useMemo(() => getRowColFromString(knightLocation), [knightLocation]);
+
+	// Get coordinates of mouse position within the ref element
+	const { ref, x, y } = useMouse();
+
+	const handleGridClick: React.MouseEventHandler<HTMLDivElement> = () => {
+		const col = Math.floor(x / 110);
+		const row = Math.floor(y / 110);
+
+		if (knightSelected) {
+			if (checkValidMove({ board, from: knightPosition, to: [row, col] })) {
+				const newBoard = [...board];
+				newBoard[row][col] = -1;
+				setBoard(newBoard);
+				setKnightLocation(getStringFromRowCol(row, col));
+			} else {
+				alert("Invalid move");
+			}
+
+			handlers.close();
+			return;
 		}
 
-		arr[0][0] = 1;
-		return arr;
-	});
+		if (row !== knightPosition[0] || col !== knightPosition[1]) {
+			return;
+		}
+
+		handlers.open();
+	};
 
 	return (
-		<SimpleGrid cols={8} spacing={0}>
+		<SimpleGrid cols={8} spacing={0} ref={ref} onClick={handleGridClick}>
 			{board.flat().map((square, i) => {
 				return (
 					<ChessSquare
 						key={i}
 						sqrRow={Math.floor(i / 8)}
 						sqrCol={i % 8}
-						containsKnight={square === 1}
+						containsKnight={
+							Math.floor(i / 8) === knightPosition[0] && i % 8 === knightPosition[1]
+						}
 					/>
 				);
 			})}
