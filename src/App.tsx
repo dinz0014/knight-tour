7b9e32da448d24c8fafe461651df30162bd915cb
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2023-02-09 01:01:02 +1100

    App now uses it to style its components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { Flex } from "@mantine/core";
-import "./App.css";
+import { Flex, useMantineTheme } from "@mantine/core";
+import { useViewportSize } from "@mantine/hooks";
 import MainContainer from "./components/MainContainer/MainContainer";
 import SideBar from "./components/SideBar/SideBar";
+import { CustomFonts } from "./fonts/CustomFonts";
 
 function App() {
+	const { width, height } = useViewportSize();
+	const theme = useMantineTheme();
+
 	return (
-		<Flex gap={0}>
-			<SideBar />
-			<MainContainer />
-		</Flex>
+		<>
+			<CustomFonts />
+			<Flex
+				gap={0}
+				style={{
+					backgroundColor: theme.colors.backdropColor[theme.fn.primaryShade()],
+					width: `${width}px`,
+					height: `${height}px`,
+				}}
+			>
+				<SideBar />
+				<MainContainer />
+			</Flex>
+		</>
 	);
 }
 
