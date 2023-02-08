51dca708587deeb66a3c64f9045447552e445516
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2023-02-08 22:21:47 +1100

    Made the validation logic more elegant and removed unnecessary css file

--- src/api/knightsTour.ts
+++ src/api/knightsTour.ts
@@ -24,15 +24,18 @@ export function getValidMoves({board, knightPosition} : GetValidMovesArgs): [num
     });
 
     return validMoves;
-};
+}
 
 export function checkValidMove({board, from, to} : CheckValidMoveArgs) : boolean {
+    // Check if the new position is within the bounds of the board and not visited
     const [fr, fc] = from;
     const [tr, tc] = to;
-    // Check if the new position is within the bounds of the board and not visited
     const isToValid = (tr >= 0 && tr < board.length && tc >= 0 && tc < board[0].length && board[tr][tc] !== -1);
 
     // Check if the row and column differences matchup (either rd = 2 and cd = 1 or vice versa)
-    const isMoveValid = (Math.abs(tr - fr) === 2 && Math.abs(tc - fc) === 1) || (Math.abs(tr - fr) === 1 && Math.abs(tc - fc) === 2);
+    const rd = Math.abs(fr - tr);
+    const cd = Math.abs(fc - tc);
+    const isMoveValid = rd <= 2 && cd <= 2 && rd + cd === 3;
+
     return isMoveValid && isToValid;
 }
