25798877a4b982ae3dd6cfeaeb1cc35b346986e8
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2023-02-09 17:02:03 +1100

    Got rid of unnecessary Center wrappers

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -10,35 +10,29 @@ const SideBar = (): JSX.Element => {
 
 	return (
 		<Stack className={classes.root}>
-			<Center>
-				<Title className={classes.title} color={textColor}>
-					Knight's Tour
-				</Title>
-			</Center>
-			<Center style={{ flexDirection: "column" }}>
-				<Text className={classes.welcome} color={textColor}>
-					Welcome to Knight's Tour!
-				</Text>
-				<Text className={classes.description} color={textColor}>
-					Instructions on how to play the game are below.
-				</Text>
-				<List className={classes.instructions} type="ordered">
-					<List.Item>
-						Click "START" and provide a starting square for the Knight
-					</List.Item>
-					<List.Item>
-						You can move your Knight by first clicking on the square that the Knight is
-						at, and then selecting another square for the Knight to move to.
-					</List.Item>
-					<List.Item>The Knight can only move in an L shape.</List.Item>
-					<List.Item>The Knight cannot visit the same square twice.</List.Item>
-					<List.Item>Your goal is to visit as many squares as you can.</List.Item>
-					<List.Item>
-						The game ends when you either visit all the squares or the Knight runs out
-						of valid moves.
-					</List.Item>
-				</List>
-			</Center>
+			<Title className={classes.title} color={textColor}>
+				Knight's Tour
+			</Title>
+			<Text className={classes.welcome} color={textColor}>
+				Welcome to Knight's Tour!
+			</Text>
+			<Text className={classes.description} color={textColor}>
+				Instructions on how to play the game are below.
+			</Text>
+			<List className={classes.instructions} type="ordered">
+				<List.Item>Click "START" and provide a starting square for the Knight</List.Item>
+				<List.Item>
+					You can move your Knight by first clicking on the square that the Knight is at,
+					and then selecting another square for the Knight to move to.
+				</List.Item>
+				<List.Item>The Knight can only move in an L shape.</List.Item>
+				<List.Item>The Knight cannot visit the same square twice.</List.Item>
+				<List.Item>Your goal is to visit as many squares as you can.</List.Item>
+				<List.Item>
+					The game ends when you either visit all the squares or the Knight runs out of
+					valid moves.
+				</List.Item>
+			</List>
 			<ButtonBar />
 		</Stack>
 	);
