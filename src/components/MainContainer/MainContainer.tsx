4fcae02af62ed57a6ee7db6f96060159014e1236
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2023-02-09 17:06:53 +1100

    Fixed MainContainer to pass in size parameters into ChessBoard. Also fixed styling for MainContainer

--- src/components/MainContainer/MainContainer.tsx
+++ src/components/MainContainer/MainContainer.tsx
@@ -1,11 +1,24 @@
 import { Center } from "@mantine/core";
+import { useElementSize } from "@mantine/hooks";
 import React from "react";
 import ChessBoard from "../ChessBoard/ChessBoard";
 
 const MainContainer = () => {
+	const { ref, width, height } = useElementSize();
+	const boardWidth = width - 50;
+	const boardHeight = height - 50;
 	return (
-		<Center>
-			<ChessBoard />
+		<Center
+			ref={ref}
+			style={{
+				flex: 3,
+				height: "100vh",
+			}}
+		>
+			<ChessBoard
+				maxSize={boardWidth < boardHeight ? boardWidth : boardHeight}
+				boardSize={8}
+			></ChessBoard>
 		</Center>
 	);
 };
