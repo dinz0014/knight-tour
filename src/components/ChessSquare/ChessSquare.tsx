420fac89c01f7afe14c053089f13974b0b6d2237
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2023-02-24 21:58:45 +1100

    Added logic to handle the clicks on a square. Notice that now we don't have to keep track of the mouse location and each square is responsible for its own click event.

--- src/components/ChessSquare/ChessSquare.tsx
+++ src/components/ChessSquare/ChessSquare.tsx
@@ -5,9 +5,32 @@ import { ChessSquareProps } from "./types";
 
 const ChessSquare = (props: ChessSquareProps) => {
 	const { classes } = useStyles(props);
+
+	const { sqrRow, sqrCol, containsKnight, sqrSize, knightSelected, makeMove, setKnightSelected } =
+		props;
+
+	const handleSquareClick = () => {
+		// Square has the knight, and it has not been selected yet.
+		if (!knightSelected && containsKnight) {
+			setKnightSelected(true);
+			return;
+		}
+
+		// Trying to move the knight to this square
+		if (!containsKnight && knightSelected) {
+			makeMove(sqrRow, sqrCol);
+			return;
+		}
+
+		// Trying to unselect the knight
+		if (containsKnight && knightSelected) {
+			setKnightSelected(false);
+		}
+	};
+
 	return (
-		<div className={classes.root}>
-			{props.containsKnight ? <KnightPiece size={props.sqrSize}></KnightPiece> : null}
+		<div className={classes.root} onClick={handleSquareClick}>
+			{containsKnight ? <KnightPiece size={sqrSize}></KnightPiece> : null}
 		</div>
 	);
 };
