69e1b307c750bf661c9e2e6b1bef7153b15ca5a6
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2023-03-06 20:54:08 +1100

    Moved the buttons from ButtonBar into SideBar. Also removed the instructions as it clogs the UI. Didn't see the need for a separate component to hold the buttons.

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,9 +1,9 @@
-import { Center, List, Stack, Text, Title, useMantineTheme } from "@mantine/core";
+import { Button, Center, List, Stack, Text, Title, useMantineTheme } from "@mantine/core";
 import React from "react";
-import ButtonBar from "../ButtonBar/ButtonBar";
+import { PrimaryComponentProps } from "../../types";
 import useStyles from "./style";
 
-const SideBar = (): JSX.Element => {
+const SideBar = (props: PrimaryComponentProps): JSX.Element => {
 	const theme = useMantineTheme();
 	const textColor = theme.colorScheme === "dark" ? "white" : "black";
 	const { classes } = useStyles();
@@ -16,24 +16,13 @@ const SideBar = (): JSX.Element => {
 			<Text className={classes.welcome} color={textColor}>
 				Welcome to Knight's Tour!
 			</Text>
-			<Text className={classes.description} color={textColor}>
-				Instructions on how to play the game are below.
-			</Text>
-			<List className={classes.instructions} type="ordered">
-				<List.Item>Click "START" and provide a starting square for the Knight</List.Item>
-				<List.Item>
-					You can move your Knight by first clicking on the square that the Knight is at,
-					and then selecting another square for the Knight to move to.
-				</List.Item>
-				<List.Item>The Knight can only move in an L shape.</List.Item>
-				<List.Item>The Knight cannot visit the same square twice.</List.Item>
-				<List.Item>Your goal is to visit as many squares as you can.</List.Item>
-				<List.Item>
-					The game ends when you either visit all the squares or the Knight runs out of
-					valid moves.
-				</List.Item>
-			</List>
-			<ButtonBar />
+			<Stack>
+				<Button>RESET</Button>
+				<Button>UNDO</Button>
+				<Button>HINT</Button>
+				<Button>SOLVE</Button>
+				<Button>HELP</Button>
+			</Stack>
 		</Stack>
 	);
 };
